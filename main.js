752b61b4a50e71cda5ac6ead73037e2fde9d46d8
jenni hynynen <jennihynynen80@gmail.com>
2022-03-23 12:31:50 +0200

    locked func

--- main.js
+++ main.js
@@ -41,6 +41,7 @@ var bet2 = 2;
 var bet3 = 3;
 var money = 50;
 var price = 0;
+var lockedRoll = 0
 // kuvan arvonta
 const images = ['arryn.png' , 'lannister.png' , 'stark.png' , 'targaryen.png' , 'jackpot.png']
 const locks = [0 , 0 , 0 , 0]
@@ -57,15 +58,17 @@ function stop(lockIndex){
    }
 } 
 
-
-
-
 function roll(){
     if (locks[0] == 0){
         let i = Math.floor(Math.random() * 5)
         slots[0] = i
         document.getElementById('1').src = `./img/${images[i]}`
     }
+    if (locks[0] != 0){
+        lockedRoll = lockedRoll + 1
+          
+    } 
+   
     if (locks[1] == 0){
         let x = Math.floor(Math.random() * 5)
         slots[1] = x
@@ -81,11 +84,15 @@ function roll(){
         slots[3] = a
         document.getElementById('4').src = `./img/${images[a]}`
     }
-
+    if (lockedRoll > 1){
+        locks[lockIndex] = 0
+    } 
+   
  
     winnings();
     youWon();
 }
+
 
 
 
@@ -127,7 +134,48 @@ function youWon(){
             triggerModal();
         }
     }
+    if (slots[0] == slots[1] && slots[1] == slots[2] && slots[2] != slots[3] ){
+        if (slots[0] == 4){        
+            money = money + bet * 5
+            price = bet * 5
+            renderPrice();
+            renderMoney();
+            triggerModal();
+        }
+    }
+    if (slots[1] == slots[2] && slots[2] == slots[3] && slots[0] != slots[1] ){
+        if (slots[1] == 4){        
+            money = money + bet * 5
+            price = bet * 5
+            renderPrice();
+            renderMoney();
+            triggerModal();
+        }
+    }
+    if (slots[2] == slots[3] && slots[3] == slots[0] && slots[1] != slots[2] ){
+        if (slots[0] == 4){        
+            money = money + bet * 5
+            price = bet * 5
+            renderPrice();
+            renderMoney();
+            triggerModal();
+        }
+    }
+    if (slots[0] == slots[1] && slots[1] == slots[3] && slots[1] != slots[2] ){
+        if (slots[0] == 4){        
+            money = money + bet * 5
+            price = bet * 5
+            renderPrice();
+            renderMoney();
+            triggerModal();
+        }
+    }
 }
+
+
+
+
+
 
 function winnings(){
     money = money - bet;
